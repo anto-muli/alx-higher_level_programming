d8235c148515fd5d18c706fc797fb915814b0597
anto-muli <memuli@students@uonbi.ac.ke>
2023-11-21 13:26:54 +0300

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-# 0-square.py by UDO INNOCENT
-"""A module that defines a square in the code """
+"""
+    square class
+"""
 
 
 class Square:
-    """A class that reps in a square"""
+    """Define a square"""
 
     def __init__(self, size=0):
-        """Initializes  square class in the code
-        Args:
-            size: reps the size of the square defined
-        Raises:
-            TypeError: if size isn't int
-            ValueError: if size is < zero
-        """
-
-        if not isinstance(size, int):
-            raise TypeError('size must be an int')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
