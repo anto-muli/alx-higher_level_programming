abe9daf50243f50b8f519983ee97b2ee8bd557e5
anto-muli <memuli@students@uonbi.ac.ke>
2023-11-21 13:11:34 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by UDO INNOCENT
+"""Defines a square function in the ^2 """
+
+
+class Square:
+    """Reps a square"""
+
+    def __init__(self, size):
+        """Initializes this square class in the class
+        Args: size - reps the size of the square defined
+        """
+
+        self.__size = size
