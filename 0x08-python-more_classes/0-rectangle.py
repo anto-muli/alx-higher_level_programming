d13bbc4ac2e45b5b12fe108a5e277a93b844e6fb
anto-muli <memuli@students@uonbi.ac.ke>
2023-11-27 19:04:42 +0300

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""A class that defines a rectangle"""
+"""A class to define a rectangle in program"""
 
 
 class Rectangle:
-    """An empty class that represents a rectangle"""
+    """An empty class that reps a rectangle in code"""
     pass
