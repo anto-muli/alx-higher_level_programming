1036c460cf1e8a5332673e71f8fe070274450674
anto-muli <memuli@students@uonbi.ac.ke>
2023-11-28 11:14:25 +0300

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,9 +50,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rect = []
-        for i in range(self.__height):
-        [rect.append('#') for j in range(self.__width)]
-        if i != self.__height - 1:
-            rect.append("\n")
-        return ("".join(rect))
+        rectangle = ""
+	for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+        return (rectangle)
