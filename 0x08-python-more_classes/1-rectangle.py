342adb7cf24607ba7b9ccd5d541ec2aff5909858
anto-muli <memuli@students@uonbi.ac.ke>
2023-11-28 10:17:21 +0300

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
+class Rectangle:
+    """this reps a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Starts this rectangle class
+        Args:
+            width: reps the width of the rectangle
+            height: reps the height of the rectangle
+        Raises:
+            TypeError: if size is not int
+            ValueError: if size is < zero
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieves width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """gets width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an int")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """gets height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an int")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
